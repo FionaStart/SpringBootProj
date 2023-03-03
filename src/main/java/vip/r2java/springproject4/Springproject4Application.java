9e89bbdac0efe060b0c603addff8de117a30c5cc
fangzhou <lifangzhou1011@gmail.com>
2023-03-03 14:06:14 +0800

    first commit

---
+++ src/main/java/vip/r2java/springproject4/Springproject4Application.java
@@ -0,0 +1,13 @@
+package vip.r2java.springproject4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springproject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springproject4Application.class, args);
+	}
+
+}
---
+++ src/test/java/vip/r2java/springproject4/Springproject4ApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.r2java.springproject4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproject4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
